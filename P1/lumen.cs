778ef2fb24b99587f0266ac34de8b2c67f43444b
kroscoe45 <56187219+kroscoe45@users.noreply.github.com>
2023-04-03 15:47:44 -0700

    refactor lumen class

--- P1/lumen.cs
+++ P1/lumen.cs
@@ -1,130 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/* CLASS INVARIANTS
- * 'power' variable will always be an integer between POWER_MIN and POWER_MAX
- * The array 'initialValues' will not change after the object has been created
- */
-
-/* QUESTIONS
- * 1. Do we need an active state to initialize the lumen object to active, or can we 
- * initialie it to any non-inactive state such as stable? Would it be more optimal to
- * have two booleans rather than four enums so that an object can be both active and 
- * stable?
- * 2.
- */
-
-/*DESIGN CHOICES
- * 1.Power is supplied directly to the lumen object, such that there is no way a 
- * lumen object can be switched off if the power is above the POWER_MIN constant.
- * Because of this we do not need an INACTIVE state.
- * 2.The number returned by the glow() function when the state is ERRATIC approaches
- * the number that will be returned when state is stable. This way there can be
- * a smooth transition from low to high power without any sharp jumps at full
- * power.
- * 3.
- * 
- * 
- */
-
-
-//brightness: arbitrary maximum brightness that the object is capable of producing when full power
-//power: the percent that the lumen object is powered (0 - 100)
-//size: how large the lumen object is
 namespace P1
 {
+	public class zappedErraticObject : Exception {
+		public zappedErraticObject(){}
+		public zappedErraticObject(string message) : base(message) { }
+
+	}
 	public class lumen
 	{
-		private const int POWER_DECREMENT = 1;
-        private const int POWER_MAX = 100;
-        private const int POWER_MIN = 0;
-        private const int BRIGHTNESS_DECREMENT = 1;
-        private const int GLOWCOUNT_THRESHHOLD = 10;
-		enum States
+		private enum States
 		{
 			INACTIVE,
 			STABLE,
 			ERRATIC
 		}
 		private States state;
-        private int glowRequestCount;
-
-        private readonly int[] initialValues = new int[3]; //[size, brightness, power]
-        private readonly int size;
-        private int brightness;
-		private int power;
+    private int glowCount;
+		private readonly int size;
+		private readonly int initialPower;
+		private readonly int maxStablePower;
+		private readonly int initialBrightness;
+		private readonly int resetThreshold;
+		private readonly int materialCapacity; //how much power the object can hold per 1 size before becoming erratic
 		private int dimness;
-		public lumen(int _size = 10, int _brightness = 100, int _power = 100)
+		private int brightness;
+		private int power;
+		public lumen(int _size = 10, int _power = 100, int _materialCapacity = 10)
 		{
-            initialValues[0] = size = _size;
-			initialValues[1] = _brightness;
-			initialValues[2] = _power;
-            init();
+			state = States.STABLE;
+      size = _size;
+			power = initialPower = _power;
+			setBrightness();
+			initialBrightness = brightness;
+			resetThreshold = Math.Clamp(power, 1, power);
+			materialCapacity = _materialCapacity;
+			maxStablePower = materialCapacity * size;
 		}
-		private void init()
-		{
-            brightness = initialValues[1];
-			power = 0;
-            setPower(initialValues[2]);
-			dimness = POWER_MAX - power;
-            state = States.STABLE;
-        }
-		private void setPower(int newPower)
-		{
-			power = Math.Clamp(newPower, POWER_MIN, POWER_MAX);
-			switch (power)
-			{
-				case POWER_MIN:
-					state = States.INACTIVE;
-					break;
-				case POWER_MAX:
-					state = States.STABLE;
-					break;
-				default:
-					state = States.ERRATIC;
-					break;
-			}
+		private void setBrightness() {
+			brightness = size * power;
+			dimness = initialBrightness / Math.Clamp(brightness, 1, brightness);
+		}
+		private void updateState() {
+			state = States.INACTIVE;
+			if (power >=0)
+				state = States.STABLE;
+			if (power >= materialCapacity * size)
+				state = States.ERRATIC;
 		}
 		public int glow()
 		{
-			setPower(power - POWER_DECREMENT);
-			glowRequestCount++;
+			glowCount++;
+			power--;
+			updateState();
+			setBrightness();
 			if (state == States.INACTIVE)
-                return dimness;
+				return dimness;
 			else if (state == States.STABLE)
-				return size * brightness;
-
-			//as the state approaches stable (fully powered) the return value
-			//will also approach the return value for the stable state
-			else
-				return (power / POWER_MAX) * size * brightness;
+				return brightness;
+			else {
+				return power % 2 == 0 ? (maxStablePower * size) / 2 : brightness;
+			}
 		}
-		public void reset()
+		public bool reset()
 		{
-			if (glowRequestCount > GLOWCOUNT_THRESHHOLD && power > 0)
-				init();
-			else
-				brightness -= BRIGHTNESS_DECREMENT;
+			updateState();
+			if (glowCount > resetThreshold && power > 0) {
+				state = States.STABLE;
+				power = initialPower;
+				setBrightness();
+				return true;
+			}
+			else {
+				power--;
+				setBrightness();
+				return false;
+			}
 		}
-		public bool isActive()
-		{
-			return state == States.INACTIVE ? false : true;
+		public bool isActive() { return state != States.INACTIVE; }
+		public bool isStable() { return state == States.STABLE; }
+		public void zap() {
+			updateState();
+			if (state == States.ERRATIC)
+				throw new zappedErraticObject("Cannot zap an erratic lumen object!");
+			power += initialPower;
+			updateState();
 		}
 	}
 }
-/* IMPLEMENTATION INVARIANTS
- * 1.  glow() will return numbers in a smooth pattern from low to high power
- * 2.  The reset() function will only reset the lumen object to the original
- *     values if glow() has been called more than GLOWCOUNT_THRESHOLD times
- *     the state will be INACTIVE of the power is POWER_MIN, STABLE if the
- * 3.  power is POWER_MAX, and ERRATIC if it is between those two values. The
- *     value of power can never not be between or equal to POWER_MIN and
- *     POWER_MAX.
- * 4.  The 'dimness' variable is updated to be  the inverse of the 'power' 
- *     variable. As power decreases the dimness increases
- * 5.  The 'brightness' variable can be negative (presumably the lumen obj
- *     will be emitting shadows(?). There is no upper or lower bound for the
- *     brightness of the object.
- */
